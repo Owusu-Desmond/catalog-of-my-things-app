436ac2c61a05511f0c8fc4cff696e0532c4345e5
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-10 19:39:39 +0500

    Create Item class and its properties

---
+++ item.rb
@@ -0,0 +1,25 @@
+class Item
+  attr_accessor :publish_date, :genre, :author, :source, :label
+
+  def initialize(id, publish_date, archived)
+    @id = id
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
+  end
+end
