a4749086980189483d83735cd21c94b2380c7980
Mesi21 <mse212002@gmail.com>
2022-10-11 18:14:44 +0300

    :rotating_light: fix rubocop errors

--- label.rb
+++ label.rb
@@ -1,9 +1,9 @@
 class Label
   attr_accessor :title, :color
   attr_reader :items
-  
+
   def initialize(title, color)
-    @id = Random.rand(1...10000)
+    @id = Random.rand(1...10_000)
     @title = title
     @color = color
     @items = []
