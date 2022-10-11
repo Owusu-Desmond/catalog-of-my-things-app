5f1996123bedfff177d82405fc9c5a23ea0d5fa9
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-11 14:44:41 +0500

    Add properties of game class

--- game/game.rb
+++ game/game.rb
@@ -1,6 +1,11 @@
+require 'date'
 require_relative '../item'
 
 class Game < Item
-  def initialize
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
   end
 end
