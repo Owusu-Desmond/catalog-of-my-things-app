9f042dd966a6e5f6c706d50ca2ce0b0513d1f211
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-11 20:12:22 +0500

    Fix linters error

--- game/game.rb
+++ game/game.rb
@@ -4,7 +4,7 @@ require_relative '../item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer, last_played_at, publish_date, id=nil, archived: false)
+  def initialize(multiplayer, last_played_at, publish_date, id = nil, archived: false)
     super(publish_date, id, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
@@ -12,10 +12,6 @@ class Game < Item
 
   def can_be_archived?
     date_diff = Date.today.year - @last_played_at.year
-    if super && date_diff > 2
-      return true
-    else
-      return false
-    end
+    super && date_diff > 2
   end
 end
