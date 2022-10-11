1ed03b40d7b1a0d995a9c42d302becbecf3fdae5
Mesi21 <mse212002@gmail.com>
2022-10-11 11:13:32 +0300

    :sparkles: add initialize method to book class

--- book.rb
+++ book.rb
@@ -1,9 +1,13 @@
 require 'item'
 
 class Book < Item
-  def initialize
+  def initialize(publisher, :publish_date, cover_state = '')
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
   end
 
-  def can_be_archived
+  def can_be_archived?
+    
   end
 end
