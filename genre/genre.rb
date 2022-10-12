0ee5cf0c4bafe5d881629384b3f362cf8d9f25a8
Owusu-Desmond <desmond0406114009@gmail.com>
2022-10-12 09:37:02 +0000

    Fix

--- genre/genre.rb
+++ genre/genre.rb
@@ -1,26 +1,27 @@
-require_relative 'item'
+require_relative '../item'
 
+# class for genre
 class Genre < Item
-    attr_accessor :name
+  attr_accessor :name
 
-    def initialize(id, name)
-        super(id, nil, nil)
-        @name = name
-        @items = []
-    end
+  def initialize(id, name)
+    super(id, nil, nil)
+    @name = name
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.add_genre(self)
-        puts "Item #{item} has been added successfully with this genre!"
-    end
+  def add_item(item)
+    @items << item
+    item.add_genre(self)
+    puts "Item #{item} has been added successfully with this genre!"
+  end
 
-    def list_genre
-        if @items.empty?
-            puts "There is no available items with this genre."
-            return
-        end
-        puts "All available genres:"
-        @items.each_with_index {|item, index| puts "#{index + 1}. #{item} - #{item.genre}"}
+  def list_genre
+    if @items.empty?
+      puts 'There is no available items with this genre.'
+      return
     end
+    puts 'All available genres:'
+    @items.each_with_index { |item, index| puts "#{index + 1}. #{item} - #{item.genre}" }
+  end
 end
--- music_album/music_album.rb
+++ music_album/music_album.rb
@@ -1,37 +1,37 @@
-require_relative 'item'
+require_relative '../item'
+# class for music album
 class MusicAlbum < Item
-    attr_accessor :on_spotify,
+  attr_accessor :on_spotify
 
-    def initialize(on_spotify)
-        @on_spotify = on_spotify
-        @music_albums = []
-    end
+  def initialize(on_spotify)
+    @on_spotify = on_spotify
+    @music_albums = []
+  end
 
-    def can_be_archived?(date)
-        super(date) && on_spotify
-    end
+  def can_be_archived?(date)
+    super(date) && on_spotify
+  end
 
-    def add_music_album
-        print "Enter name of music album: "
-        music_album = get.chomp
-        # if music album exists
-        if @music_album.find{|x| x == music_album}
-            puts "Music album already exists"
-            add_music_album
-        else
-            @music_album << music_album
-            puts "Music album added"
-        end
+  def add_music_album
+    print 'Enter name of music album: '
+    music_album = get.chomp
+    # if music album exists
+    if @music_album.find { |x| x == music_album }
+      puts 'Music album already exists'
+      add_music_album
+    else
+      @music_album << music_album
+      puts 'Music album added'
     end
+  end
 
-    def list_music_albums
-        # if there is no music ablum
-        if @music_albums.empty?
-            puts "There are no available music albums."
-            return
-        end
-        puts "All music albums:"
-        @music_albums.each_with_index {|music_album, index| puts "#{index + 1}. #{music_album}"}
+  def list_music_albums
+    # if there is no music ablum
+    if @music_albums.empty?
+      puts 'There are no available music albums.'
+      return
     end
+    puts 'All music albums:'
+    @music_albums.each_with_index { |music_album, index| puts "#{index + 1}. #{music_album}" }
+  end
 end
-
