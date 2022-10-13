8e8120d59baa55b1d826ded3bc2abf0a82ffe28b
Mesi21 <mse212002@gmail.com>
2022-10-13 15:47:19 +0300

    :bug: fix Book class initialize method to get the correct data

--- book/book.rb
+++ book/book.rb
@@ -4,7 +4,7 @@ class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publisher, publish_date, cover_state = '')
-    super(id, publish_date, archived)
+    super(publish_date, id, archived)
     @publisher = publisher
     @cover_state = cover_state
   end
