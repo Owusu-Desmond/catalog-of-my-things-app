b3c876051c289dd4165eea7e9e3094301c4c501d
Owusu-Desmond <desmond0406114009@gmail.com>
2022-10-13 09:46:49 +0000

    Modify item class

--- item.rb
+++ item.rb
@@ -1,43 +1,44 @@
 require 'date'
 # class for Item
 class Item
-  attr_accessor :publish_date, :genre, :author, :source, :label
+  attr_accessor :publish_date
+  attr_reader :id, :author, :genre, :source, :label
 
-  def initialize(id, publish_date, archived)
-    @id = id
+  def initialize(publish_date, id = nil, archived: false)
+    @id = id || Random.rand(101..1000)
     @publish_date = publish_date
     @archived = archived
   end
 
-  def add_genre(genre)
-    @genre = genre
-  end
-
-  def add_author(author)
+  def author=(author)
     @author = author
+    author.items << self unless author.items.include?(self)
   end
 
-  def add_source(source)
-    @source = source
-  end
-
-  def add_label(label)
+  def label=(label)
     @label = label
+    label.items << self unless author.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source
+    source.items << self unless source.items.include?(source)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(genre)
   end
 
   private
 
-  def can_be_achieved(date)
+  def can_be_archived?(date)
     date = Date.parse(date).year
     data_now = Date.today.year
     (data_now - date) > 10
   end
 
   def move_to_archive
-    if can_be_achieved
-      true
-    else
-      false
-    end
+    can_be_archived?
   end
 end
