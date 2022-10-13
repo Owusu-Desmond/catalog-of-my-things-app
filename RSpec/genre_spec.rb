5bad41a16f994547770ce4b5a768fcf498d7a68c
Owusu-Desmond <desmond0406114009@gmail.com>
2022-10-13 09:49:07 +0000

    Write test for genre methods

---
+++ RSpec/genre_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../genre/genre'
+require_relative '../item'
+
+describe Genre do
+  describe '#new' do
+    genre = Genre.new(1, 'Jazz')
+    it 'intializes the genres correctly' do
+      expect(genre.name).to eq('Jazz')
+    end
+  end
+
+  describe '#add_item' do
+    genre = Genre.new(1, 'Jazz')
+    item = Item.new('25/08/2010', 1)
+    genre.add_item(item)
+    it 'checks if the item was added successfuly' do
+      expect(genre.list_genre.size).to eq(2)
+    end
+  end
+
+  describe '#list_genre' do
+    genre = Genre.new(1, 'Jazz')
+    item = Item.new(genre, '27/07/2020', archived: false)
+    item2 = Item.new(genre, '25/08/2010', archived: false)
+    genre.add_item(item)
+    genre.add_item(item2)
+    it 'Lists all the items in that genre, returns an error if there are none' do
+      expect(genre.list_genre.size).to eq(4) # 1 for name of the genre and 2*2 for placeholders, 2 for item instances like this:
+    end
+  end
+end
