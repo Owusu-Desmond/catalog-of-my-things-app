e9793f3db6830e9a66d5ef2543e02e706962a67c
Owusu-Desmond <desmond0406114009@gmail.com>
2022-10-14 16:19:36 +0000

    Update app class

--- app.rb
+++ app.rb
@@ -2,24 +2,41 @@ require_relative 'game/game'
 require_relative 'game/game_module'
 require_relative 'author/author'
 require_relative 'author/author_module'
+require_relative 'music_album/music_album'
+require_relative 'music_album/music_album_module'
+
+require_relative 'genre/genre'
+require_relative 'genre/genre_module'
+require_relative 'preserve'
+require 'securerandom'
+# require_relative 'music_album-genre-actions'
+# require_relative 'start_preserve_actions'
 
 # class application
 class App
   def initialize
+    @preserve = Preserve.new
     @games = []
     @authors = []
   end
 
   include GameModule
   include AuthorModule
+  include MusicAlbumModule
+  include GenreModule
 
   def run
+    @preserve.load_genre_data
+    @preserve.load_music_albums_data
+    puts @preserve.music_albums
     puts 'Welcome to Catalog my thing'
     loop do
       menu_message
       print 'Enter your option: '
       option = gets.chomp.to_i
       if option == 7
+        @preserve.store_music_album_data
+        @preserve.store_genre_data
         puts 'Thanks for using the app'
         break
       end
@@ -30,9 +47,9 @@ class App
   def menu_message
     puts '--------------------------------'
     puts 'Welcome to my catalog app'
-    puts '1: This is the first option'
-    puts '2: This is the second option'
-    puts '3: This is the third option'
+    puts '1: List all genres (e.g \'Comedy\', \'Thriller\')'
+    puts '2: List all music albums'
+    puts '3: Add a music album'
     puts '4: List all authors'
     puts '5: List of games'
     puts '6: Add a game'
@@ -48,11 +65,11 @@ class App
   def options(option)
     case option
     when 1
-      puts 'You puts in the first option'
+      list_genres
     when 2
-      puts 'You puts in the second option'
+      list_music_albums
     when 3
-      puts 'You puts in the third option'
+      add_music_album
     when 4
       puts list_authors
     when 5
