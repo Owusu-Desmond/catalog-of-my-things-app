4e25d90bc0acb23de634cea9bf493fbfdbfebf86
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-14 16:47:25 +0500

    Update author_module to save author id

--- author/author_module.rb
+++ author/author_module.rb
@@ -1,12 +1,12 @@
 module AuthorModule
   def add_author(author)
-    @authors.push({ 'first_name' => author.first_name, 'last_name' => author.last_name })
+    @authors.push({ 'id' => author.id, 'first_name' => author.first_name, 'last_name' => author.last_name })
   end
 
   def list_authors
     puts "There is no author in the list\n" if @authors.empty?
     @authors.each_with_index do |author, index|
-      puts "#{index}) First Name: #{author['first_name']} Last Name: #{author['last_name']}"
+      puts "#{index}) ID: #{author['id']} First Name: #{author['first_name']} Last Name: #{author['last_name']}"
     end
   end
 
@@ -15,7 +15,7 @@ module AuthorModule
     data = []
 
     authors.each do |author|
-      data.push({ first_name: author['first_name'], last_name: author['last_name'] })
+      data.push({ id: author['id'], first_name: author['first_name'], last_name: author['last_name'] })
     end
 
     File.write(file_name, JSON.generate(data))
@@ -28,8 +28,8 @@ module AuthorModule
     if File.exist?(file_name) && File.read(file_name) != ''
       file_data = JSON.parse(File.read(file_name))
       file_data.each do |author|
-        author = Author.new(author['first_name'], author['last_name'])
-        data.push({ 'first_name' => author.first_name, 'last_name' => author.last_name })
+        author = Author.new(author['first_name'], author['last_name'], author['id'])
+        data.push({ 'id' => author.id, 'first_name' => author.first_name, 'last_name' => author.last_name })
       end
     end
     data
