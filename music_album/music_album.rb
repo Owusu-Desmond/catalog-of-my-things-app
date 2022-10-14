4156ccf295011877628b28346f94ab7749174da6
Owusu-Desmond <desmond0406114009@gmail.com>
2022-10-14 17:21:39 +0000

    Fix

--- music_album/music_album.rb
+++ music_album/music_album.rb
@@ -7,12 +7,12 @@ require 'json'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(id, publish_date, on_spotify)
+  def initialize(publish_date, id,  on_spotify)
     super(id, publish_date)
     @on_spotify = on_spotify
   end
 
   def can_be_archived?(date)
-    super(date) && on_spotify
+    super && @on_spotify
   end
 end
--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -17,15 +17,4 @@ describe Genre do
       expect(genre.list_genre.size).to eq(2)
     end
   end
-
-  describe '#list_genre' do
-    genre = Genre.new(1, 'Jazz')
-    item = Item.new(genre, '27/07/2020', archived: false)
-    item2 = Item.new(genre, '25/08/2010', archived: false)
-    genre.add_item(item)
-    genre.add_item(item2)
-    it 'Lists all the items in that genre, returns an error if there are none' do
-      expect(genre.list_genre.size).to eq(4)
-    end
-  end
 end
